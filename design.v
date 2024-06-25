a984237c1098cab81e33c39cb362128fb4bb2e2b
PVividPulse <125368625+VishalPV2004@users.noreply.github.com>
2024-06-25 15:26:50 +0530

    Delete design.v

--- design.v
+++
@@ -1,6 +0,0 @@
-module decimal_BCD(input [9:0] x, output [3:0] z);
-  assign z[0] = x[1] | x[3] | x[5] | x[7] | x[9];
-  assign z[1] = x[2] | x[3] | x[6] | x[7];
-  assign z[2] = x[4] | x[5] | x[6] | x[7];
-  assign z[3] = x[8] | x[9];
-endmodule;
